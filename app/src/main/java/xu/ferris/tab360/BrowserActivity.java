790a8e70ea962f6123e7a6a758c22459d962a5e8
xufeifandj <459821731@qq.com>
2016-01-24 12:20:47 +0800

    高仿360浏览器tab效果

---
+++ app/src/main/java/xu/ferris/tab360/BrowserActivity.java
@@ -0,0 +1,16 @@
+package xu.ferris.tab360;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 高仿360浏览器tab效果
+ */
+public class BrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+    }
+}
